56d08199061e8e111d8985efbe556258127c2085
Simon <ghdud0503@gmail.com>
2021-02-08 00:10:10 +0900

    logo link

--- workspaces/client/src/components/header.tsx
+++ workspaces/client/src/components/header.tsx
@@ -19,7 +19,9 @@ export const Header:React.FC = () => {
             )}
             <header className="py-4">
                 <div className="w-full px-5 xl:px-0 max-w-screen-2xl mx-auto flex justify-between items-center">
-                <img src={logo} className="w-24" alt="logo"/>
+                <Link to="/">
+                    <img src={logo} className="w-24" alt="logo"/>
+                </Link>
                     <span className="text-sm">
                         <Link to="/edit-profile">
                             <FontAwesomeIcon icon={faUser} className="text-xl"/>
