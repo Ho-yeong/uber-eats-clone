f8a7f0261fe04300e79b97c0ad276c787c7ee9c0
Simon <ghdud0503@gmail.com>
2021-03-02 00:18:51 +0900

    restaurant detail page

--- workspaces/client/src/components/restaurant.tsx
+++ workspaces/client/src/components/restaurant.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface IRestaurantProps {
   id: string;
@@ -7,15 +8,19 @@ interface IRestaurantProps {
   categoryName?: string;
 }
 
-export const Restaurant: React.FC<IRestaurantProps> = ({ coverImg, name, categoryName }) => {
+export const Restaurant: React.FC<IRestaurantProps> = ({ coverImg, name, categoryName, id }) => {
   return (
-    <div className="flex flex-col">
-      <div
-        style={{ backgroundImage: `url(${coverImg})` }}
-        className="bg-cover bg-center mb-3 py-20 mt-10"
-      />
-      <h3 className="text-lg font-bold">{name}</h3>
-      <span className="border-t py-2 mt-3 text-xs opacity-50 border-gray-400">{categoryName}</span>
-    </div>
+    <Link to={`/restaurant/${id}`}>
+      <div className="flex flex-col">
+        <div
+          style={{ backgroundImage: `url(${coverImg})` }}
+          className="bg-cover bg-center mb-3 py-20 mt-10"
+        />
+        <h3 className="text-lg font-bold">{name}</h3>
+        <span className="border-t py-2 mt-3 text-xs opacity-50 border-gray-400">
+          {categoryName}
+        </span>
+      </div>
+    </Link>
   );
 };
---
+++ workspaces/client/src/pages/client/restaurant.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+import { RESTAURANT_FRAGMENT } from '../../fragments';
+import { restaurant, restaurantVariables } from '../../__generated__/restaurant';
+
+const RESTAURANT_QUERY = gql`
+  query restaurant($input: RestaurantInput!) {
+    restaurant(input: $input) {
+      ok
+      error
+      restaurant {
+        ...RestaurantParts
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
+
+interface IRestaurantParams {
+  id: string;
+}
+
+export const Restaurant = () => {
+  const params = useParams<IRestaurantParams>();
+
+  const { loading, data } = useQuery<restaurant, restaurantVariables>(RESTAURANT_QUERY, {
+    variables: {
+      input: {
+        restaurantId: +params.id,
+      },
+    },
+  });
+
+  console.log(data);
+
+  return (
+    <div>
+      <div
+        className="bg-gray-600 py-48 bg-center bg-cover"
+        style={{ backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})` }}
+      >
+        <div className="bg-white w-1/4 py-8 pl-52">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mt-2">{data?.restaurant.restaurant?.category?.name}</h5>
+          <h6 className="text-sm font-light">{data?.restaurant.restaurant?.address}</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
