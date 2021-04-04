d7a82242aa113860b197e8af223be88ea7254353
Simon <ghdud0503@gmail.com>
2021-04-05 00:25:01 +0900

    cilent build script

--- workspaces/client/tailwind.config.js
+++ workspaces/client/tailwind.config.js
@@ -2,18 +2,18 @@ const { emerald } = require('tailwindcss/colors');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         lime: colors.lime,
-        emerald: colors.emerald
-      }
+        emerald: colors.emerald,
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
